99553552f6e6d0f7ff0f51009fa63183b7b3b055
Rico Kaltofen <r.kaltofen@heimrich-hannot.de>
2015-05-13 16:24:22 +0200

    fixed sql error in findMultiplePublishedMultiSRCContentElements

--- models/FileCreditModel.php
+++ models/FileCreditModel.php
@@ -14,8 +14,7 @@ class FileCreditModel extends \FilesModel
 		
 		$t = static::$strTable;
 		
-		foreach($arrCredits as $arrCredit)
-		{
+		foreach ($arrCredits as $arrCredit) {
 			$objResult = $objDatabase->prepare
 			(
 				"
@@ -23,7 +22,9 @@ class FileCreditModel extends \FilesModel
 				"
 			)->execute($arrCredit['file']);
 			
-			if ($objResult->numRows < 1) continue;
+			if ($objResult->numRows < 1) {
+				continue;
+			}
 			
 			$objResult->usage = $arrCredit['usage'];
 			
@@ -33,17 +34,14 @@ class FileCreditModel extends \FilesModel
 		return empty($arrReturn) ? null : $arrReturn;
 	}
 
-	public static function findMultiplePublishedMultiSRCContentElements($arrIds, $arrExtensions, array $arrOptions=array())
+	public static function findMultiplePublishedMultiSRCContentElements($arrIds, $arrExtensions, array $arrOptions = array())
 	{
-		if (!is_array($arrIds) || empty($arrIds) || !is_array($arrExtensions) || empty($arrExtensions))
-		{
+		if (!is_array($arrIds) || empty($arrIds) || !is_array($arrExtensions) || empty($arrExtensions)) {
 			return null;
 		}
 		
-		foreach ($arrExtensions as $k=>$v)
-		{
-			if (!preg_match('/^[a-z0-9]{2,5}$/i', $v))
-			{
+		foreach ($arrExtensions as $k => $v) {
+			if (!preg_match('/^[a-z0-9]{2,5}$/i', $v)) {
 				unset($arrExtensions[$k]);
 			}
 		}
@@ -51,106 +49,104 @@ class FileCreditModel extends \FilesModel
 		$t = static::$strTable;
 		
 		$objDatabase = \Database::getInstance();
-		
-		$objResult = $objDatabase->prepare
-		(
-			"
-				SELECT c.id AS cid, c.ptable as ptable, c.pid as parent, c.multiSRC FROM tl_content c WHERE c.multiSRC IS NOT NULL
-			"
-		)->execute();
 
-		if ($objResult->numRows < 1)
-		{
+
+		// get names of parent tables (tl_news, tl_article, tl_calendar_events)
+		$objTable = $objDatabase->prepare("SELECT DISTINCT ptable FROM tl_content")->execute();
+
+		if ($objTable->numRows < 1) {
 			return null;
 		}
-		
-		$arrReturn = null;
-		
-		while($objResult->next())
-		{
-			$arrUuids = deserialize($objResult->multiSRC, true);
-			
-			$objFiles = \FilesModel::findMultipleByUuids($arrUuids);
-			
-			if($objFiles === null) continue;
 
-			if(!$objFiles->copyright)
-			{
-				continue;
+		$arrReturn = null;
+
+		while ($objTable->next()) {
+
+			$objResult = $objDatabase->prepare
+			(
+				"
+                    SELECT c.id AS cid, c.ptable as ptable, c.pid as parent, c.multiSRC
+                    FROM tl_content c
+                    LEFT JOIN $objTable->ptable p ON p.id = c.pid
+                    WHERE c.multiSRC IS NOT NULL AND c.invisible = ''
+                "
+			)->execute();
+
+			if ($objResult->numRows < 1) {
+				return null;
 			}
 
-			if($objFiles->type == 'folder')
-			{
-				$objSubfiles = \FilesModel::findByPid($objFiles->uuid);
-				
-				if ($objSubfiles === null)
-				{
+			while ($objResult->next()) {
+				$arrUuids = deserialize($objResult->multiSRC, true);
+
+				$objFiles = \FilesModel::findMultipleByUuids($arrUuids);
+
+				if ($objFiles === null) {
 					continue;
 				}
-				
-				while ($objSubfiles->next())
-				{
-					// Skip subfolders
-					if ($objSubfiles->type == 'folder')
-					{
-						$objFolderFiles = \FilesModel::findMultipleFilesByFolder($objSubfiles->path);
-						
-						if($objFolderFiles === null)
-						{
+
+				if (!$objFiles->copyright) {
+					continue;
+				}
+
+				if ($objFiles->type == 'folder') {
+					$objSubfiles = \FilesModel::findByPid($objFiles->uuid);
+
+					if ($objSubfiles === null) {
+						continue;
+					}
+
+					while ($objSubfiles->next()) {
+						// Skip subfolders
+						if ($objSubfiles->type == 'folder') {
+							$objFolderFiles = \FilesModel::findMultipleFilesByFolder($objSubfiles->path);
+
+							if ($objFolderFiles === null) {
+								continue;
+							}
+
+							while ($objFolderFiles->next()) {
+								if (!in_array($objFolderFiles->extension, $arrExtensions)) {
+									continue;
+								}
+
+								if (!$objFolderFiles->copyright) {
+									continue;
+								}
+
+								$arrReturn[] = (object) array_merge($objResult->row(), $objFolderFiles->row());
+							}
+						}
+
+						if (!in_array($objSubfiles->extension, $arrExtensions)) {
 							continue;
 						}
-						
-						while($objFolderFiles->next())
-						{
-							if (!in_array($objFolderFiles->extension, $arrExtensions))
-							{
-								continue;
-							}
-								
-							if(!$objFolderFiles->copyright)
-							{
-								continue;
-							}
-							
-							$arrReturn[] = (object) array_merge($objResult->row(), $objFolderFiles->row());
+
+
+						if (!$objSubfiles->copyright) {
+							continue;
 						}
-					}
-				
-					if (!in_array($objSubfiles->extension, $arrExtensions))
-					{
-						continue;
-					}
 
-
-					if(!$objSubfiles->copyright)
-					{
-						continue;
+						$arrReturn[] = (object) array_merge($objResult->row(), $objSubfiles->row());
 					}
-					
-					$arrReturn[] = (object) array_merge($objResult->row(), $objSubfiles->row());
+				} else {
+					$arrReturn[] = (object) array_merge($objResult->row(), $objFiles->row());
 				}
+
 			}
-			else
-			{
-				$arrReturn[] = (object) array_merge($objResult->row(), $objFiles->row());
-			}
-			
 		}
 
 		return empty($arrReturn) ? null : $arrReturn;
 	}
 	
-	public static function findMultiplePublishedSingleSRCContentElementsByExtensions($arrIds, $arrExtensions, array $arrOptions=array())
+	public static function findMultiplePublishedSingleSRCContentElementsByExtensions($arrIds, $arrExtensions, array $arrOptions = array())
 	{
-		if (!is_array($arrIds) || empty($arrIds) || !is_array($arrExtensions) || empty($arrExtensions))
-		{
+		if (!is_array($arrIds) || empty($arrIds) || !is_array($arrExtensions) || empty($arrExtensions)) {
 			return null;
 		}
 
-		foreach ($arrExtensions as $k=>$v)
-		{
-			if (!preg_match('/^[a-z0-9]{2,5}$/i', $v))
-			{
+		foreach ($arrExtensions as $k => $v) {
+			if (!preg_match('/^[a-z0-9]{2,5}$/i', $v)) {
 				unset($arrExtensions[$k]);
 			}
 		}
@@ -164,10 +160,8 @@ class FileCreditModel extends \FilesModel
 		$addTableSql = '';
 
 		// support additional tables
-		if(is_array($GLOBALS['TL_FILECREDIT_MODELS']))
-		{
-			foreach($GLOBALS['TL_FILECREDIT_MODELS'] as $table => $autoitem)
-			{
+		if (is_array($GLOBALS['TL_FILECREDIT_MODELS'])) {
+			foreach ($GLOBALS['TL_FILECREDIT_MODELS'] as $table => $autoitem) {
 				$addTableSql .= "
 
 					UNION ALL
@@ -183,12 +177,13 @@ class FileCreditModel extends \FilesModel
 
 		$arrPalettes = $GLOBALS['TL_DCA']['tl_content']['palettes'];
 
-		$arrFlatPalettes = array('image');
+		$arrFlatPalettes  = array('image');
 		$arrImagePalettes = array();
 
-		foreach($arrPalettes as $key => $strPalette)
-		{
-			if(is_array($strPalette) || strstr($strPalette, 'addImage') === false) continue;
+		foreach ($arrPalettes as $key => $strPalette) {
+			if (is_array($strPalette) || strstr($strPalette, 'addImage') === false) {
+				continue;
+			}
 
 			$arrImagePalettes[] = $key;
 		}
@@ -202,7 +197,7 @@ class FileCreditModel extends \FilesModel
 					SELECT c.id AS cid, c.ptable as ptable, c.pid as parent, $t.*
 					FROM $t
 					LEFT JOIN tl_content c ON c.singleSRC = $t.uuid
-					WHERE (c.type IN('" . implode("','", $arrFlatPalettes) . "') OR (c.type IN('" . implode("','", $arrImagePalettes) . "') AND c.addImage = 1))
+					WHERE (c.type IN('" . implode("','", $arrFlatPalettes) . "') OR (c.type IN('" . implode("','", $arrImagePalettes) . "') AND c.addImage = 1)) AND c.invisible = ''
 
 					UNION ALL
 
@@ -217,7 +212,7 @@ class FileCreditModel extends \FilesModel
 					SELECT c.id AS cid, 'tl_news' as ptable, c.id as parent, $t.*
 					FROM $t
 					LEFT JOIN tl_news c ON c.singleSRC = $t.uuid
-					WHERE c.addImage = 1
+					WHERE c.addImage = 1 AND c.published = 1
 
 					-- support addional tables
 					$addTableSql
@@ -230,15 +225,13 @@ class FileCreditModel extends \FilesModel
 			"
 		)->execute();
 		
-		if ($objResult->numRows < 1)
-		{
+		if ($objResult->numRows < 1) {
 			return null;
 		}
 
 		$arrReturn = null;
 
-		while($objResult->next())
-		{
+		while ($objResult->next()) {
 			$arrReturn[] = (object) $objResult->row();
 		}
 
